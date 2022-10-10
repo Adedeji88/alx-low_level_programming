580f8858e062829e9e59c87de327daa5e8119385
Adedeji88 <mustaphadedeji@email.com>
2022-10-10 09:57:35 -0700

    Task 0

---
+++ alx-low_level_programming/dog.h
@@ -0,0 +1,24 @@
+#define "dog.h"
+#ifndef "dog.h"
+
+struct dog
+{
+	char * name;
+	float age;
+	char * owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+dog_t my_dog;
+{
+	char * name;
+	float age;
+	char * owner;
+};
+
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
