4d18e20c3eade92fadcc8f6044d41ecfe92d6ebc
Adedeji88 <mustaphadedeji@email.com>
2022-10-10 10:29:19 -0700

    Tasks 1-5

--- alx-low_level_programming/dog.h
+++ alx-low_level_programming/dog.h
@@ -1,7 +1,13 @@
-#define "DOG_H"
-#ifndef "DOG_H"
+#define DOG_H
+#ifndef DOG_H
+/**
+ * main - Struct dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
 
-struct dog
+struct dog;
 {
 	char * name;
 	float age;
@@ -10,14 +16,7 @@ struct dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-
 dog_t my_dog;
-{
-	char * name;
-	float age;
-	char * owner;
-};
-
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
